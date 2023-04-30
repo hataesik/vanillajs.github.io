27f2014ee3465bd8bf7b6c8cbf847bb02ce2ace8
hataesik <gkxotlr@naver.com>
2023-04-30 17:59:28 +0900

    Update background.js

--- background.js
+++ background.js
@@ -3,10 +3,9 @@ const images = ["0.jpg", "1.jpg", "2.jpeg", "3.jpg", "4.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 //const bgImage = document.createElement("img");
-
 //bgImage.src = `img/${chosenImage}`;
-
 //document.body.appendChild(bgImage);
+
 console.log(chosenImage);
 const bg = document.querySelector("body");
 
